9f31f4af668c7b2a04829087bd1cb6115f754bc5
duanedave <duanedave17@gmail.com>
2023-01-05 15:27:43 -0800

    Refactor the app component

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Sidebar from "./components/sideBar/Sidebar";
 import LogIn from "./pages/LogIn";
 import SignUp from "./pages/SignUp";
+import Home from "./pages/Home";
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
         <Routes>
           <Route path="/" element={<LogIn />} />
           <Route path="/signup" element={<SignUp />} />
+          <Route path="/home" element={<Home />} />
         </Routes>
       </Router>
     </>
