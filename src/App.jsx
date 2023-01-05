792bba4102f9555a16e022f2e59e78be442e8ab6
duanedave <duanedave17@gmail.com>
2023-01-05 12:31:06 -0800

    Restructure the app component and initialize rotes

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,19 @@
-import { BrowserRouter as Router, Routes, Route} from 'react-router-dom'
-import Sidebar from './components/sideBar/Sidebar'
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
+import Sidebar from "./components/sideBar/Sidebar";
+import LogIn from "./pages/LogIn";
 
 function App() {
-
   return (
-    <Router>
-      <div className="App">
-        <Sidebar/>
-      </div>
-    </Router>
-  )
+    <>
+      <Sidebar />
+      <Router>
+        <Routes>
+          <Route path="/" element={<LogIn />} />
+        </Routes>
+      </Router>
+    </>
+  );
 }
 
-export default App
+export default App;
