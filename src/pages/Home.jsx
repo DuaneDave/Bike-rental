b8026179792bf34963deb825c8e68a4e09245a7f
duanedave <duanedave17@gmail.com>
2023-01-06 12:22:01 -0800

    Add content in the home component

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,19 @@
 function Home() {
   return (
-    <div>
-      <h1>Home</h1>
-      <p>Home page</p>
-    </div>
+    <section>
+      <div className='home-background'></div>
+      <div className='content flex'>
+        <span>
+          <h1>Trekk</h1>
+          <h2>find your next adventure starting from here</h2>
+        </span>
+        
+        <div className='home-cta flex'>
+          <button>See Bikes</button>
+          <button>Make Reservation</button>
+        </div>
+      </div>
+    </section>
   );
 }
 
