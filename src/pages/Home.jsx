fc01d463cca3ee2ead1e91c1b279008e72ee820a
duanedave <duanedave17@gmail.com>
2023-01-07 19:39:43 -0800

    Add styles tpo the home component

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,13 +1,18 @@
 function Home() {
+  const avatar = JSON.parse(localStorage.getItem('user'));
+  
   return (
     <section>
+      <span className="avatar">
+        <img src={avatar.avatar} />
+      </span>
       <div className='home-background'></div>
       <div className='content flex'>
         <span>
-          <h1>Trekk</h1>
+          <h1>Trek</h1>
           <h2>find your next adventure starting from here</h2>
         </span>
-        
+
         <div className='home-cta flex'>
           <button>See Bikes</button>
           <button>Make Reservation</button>
