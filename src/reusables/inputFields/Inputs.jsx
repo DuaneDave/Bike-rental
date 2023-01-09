8006caa6570d61406b1529c229ffd39e53ffb0bb
duanedave <duanedave17@gmail.com>
2023-01-09 14:42:26 -0800

    Restyle the input component

--- src/reusables/inputFields/Inputs.jsx
+++ src/reusables/inputFields/Inputs.jsx
@@ -7,4 +7,14 @@ function Input({ label, type, value, onChange }) {
   );
 }
 
+function Select({ label, handleChange, ...rest }) {
+  return (
+    <div className='flex flex-column'>
+      <label htmlFor={label}>{label}</label>
+      <select onChange={handleChange} {...rest} />
+    </div>
+  );
+}
+
 export default Input;
+export { Select };
