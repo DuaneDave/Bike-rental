df38d263862899252e7e5ef4d13c998b51e060a6
Anas <anas.chaabane98@gmail.com>
2023-01-09 13:12:52 +0100

    loged in stae

--- src/app/store.js
+++ src/app/store.js
@@ -1,11 +1,13 @@
 import { configureStore } from "@reduxjs/toolkit";
 import { apiSlice } from "../components/api/apiSlice";
+import loginReducer from '../components/login/loginSlice'
 import bikeSlice from '../components/bikes/bikeSlice'
 
 export const store = configureStore({
   reducer: {
     Bikes: apiSlice.reducer,
-    toggelDetails: bikeSlice.reducer
+    toggelDetails: bikeSlice.reducer,
+    logedin: loginReducer
 
   },
   middleware: getDefaultMiddlewear => getDefaultMiddlewear().concat(apiSlice.middleware)
