4690aa1a2e65e09d55b270056971c77238e9151d
duanedave <duanedave17@gmail.com>
2023-01-09 01:21:38 -0800

    Restructure routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,25 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
-import "./index.css";
+import './index.css';
 
-import Sidebar from "./components/sideBar/Sidebar";
-import LogIn from "./pages/LogIn";
-import SignUp from "./pages/SignUp";
-import Home from "./pages/Home";
+import Sidebar from './components/sideBar/Sidebar';
+import LogIn from './pages/LogIn';
+import SignUp from './pages/SignUp';
+import Home from './pages/Home';
+import React from 'react';
 
 function App() {
   return (
-    <>
+    <React.Fragment>
       <Router>
+      <Sidebar />
         <Routes>
-          <Route path="/" element={<LogIn />} />
-          <Route path="/signup" element={<SignUp />} />
-          <Route path="/home" element={<Home />} />
+          <Route path='/' element={<LogIn />} />
+          <Route path='/signup' element={<SignUp />} />
+          <Route path='/home' element={<Home />} />
         </Routes>
       </Router>
-    </>
+    </React.Fragment>
   );
 }
 
