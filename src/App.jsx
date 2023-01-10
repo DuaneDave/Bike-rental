da7d94aa1e208432c781f527e3cc240ed4f0668e
duanedave <duanedave17@gmail.com>
2023-01-09 16:03:55 -0800

    Merge branch

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,12 @@
+import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import LogIn from "./pages/LogIn";
+import Sidebar from "./components/sideBar/Sidebar";
 import SignUp from "./pages/SignUp";
 import Home from "./pages/Home";
 import Bikes from './pages/Bikes';
 import Details from './pages/Details';
+import Reservations from "./pages/Resevation";
 import "./index.css";
 
 function App() {
@@ -17,6 +20,7 @@ function App() {
           <Route path="/home" element={<Home />} />
           <Route path="bikes"  element = {<Bikes />}/>
           <Route path='bikes/:param' element = {<Details />} />
+          <Route path="/reserve" element={<Reservations />} />
         </Routes>
       </Router>
     </React.Fragment>
