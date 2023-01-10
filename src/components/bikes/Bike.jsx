2eba93dff29d51be50f562b46b8fb786fd8d4bf8
duanedave <duanedave17@gmail.com>
2023-01-09 16:04:50 -0800

    Fix bugs in sidebar

--- src/components/bikes/Bike.jsx
+++ src/components/bikes/Bike.jsx
@@ -1,28 +1,32 @@
-import ColorSwitch from '../colorSwitch/ColorSwitch'
-import {useState} from 'react';
+import ColorSwitch from '../colorSwitch/ColorSwitch';
+import { useState } from 'react';
 import Size from './Size/Size';
-import { useParams, Link } from 'react-router-dom';
-const Bike = ({bike}) => {
-  // const {id} = useParams()
-  const [color,setColor] = useState(null)
-  const [showdetails, setShowDetails] = useState(false)
-  const changeColor = (input) => setColor(input)
-  const toggleDetails = () =>  setShowDetails(!showdetails)
+import { useParams, useNavigate } from 'react-router-dom';
 
+const Bike = ({ bike }) => {
+  const [color, setColor] = useState(null);
+  const [showdetails, setShowDetails] = useState(false);
+  const changeColor = (input) => setColor(input);
+  const toggleDetails = () => setShowDetails(!showdetails);
+
+  const navigate = useNavigate();
 
   return (
-    <div> 
-      <img onClick={() => window.location.href = `/Bikes/${bike.id}` } src={ bike.images[color] || bike.images[Object.keys(bike.images)[0]] }></img>
-      <h3>{ bike.model }</h3>
-      <p> { bike.description }</p>
-       <Size/> 
+    <div className='small'>
+      <img
+        onClick={() => navigate(`/Bikes/${bike.id}`)}
+        src={bike.images[color] || bike.images[Object.keys(bike.images)[0]]}
+      ></img>
+      <h3>{bike.model}</h3>
+      <p> {bike.description}</p>
+      <Size />
       <div>
-       {bike.colors.map(color => (
-          <ColorSwitch  key={color} changeColor={changeColor} input = {color} />
-       ))}
+        {bike.color.map((color) => (
+          <ColorSwitch key={color} changeColor={changeColor} input={color} />
+        ))}
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Bike
+export default Bike;
--- src/components/bikes/BikeList.jsx
+++ src/components/bikes/BikeList.jsx
@@ -1,26 +1,23 @@
-import Bike from './Bike'
-import Spiner from '../spiner/Spiner'
-import { useGetBikesQuery } from '../api/apiSlice'
-import { Link } from 'react-router-dom'
+import Bike from './Bike';
+import Spiner from '../spiner/Spiner';
+import { useGetBikesQuery } from '../api/apiSlice';
 
 const BikeList = () => {
   const {
-    data:Bikes,
+    data: Bikes,
     isLoading,
     isSuccess,
     isError,
-    error
-  } = useGetBikesQuery()
+    error,
+  } = useGetBikesQuery();
 
   return (
     <div>
       {isLoading && <Spiner />}
-      {isSuccess && Bikes.map(bike => (
-      <Bike bike={bike} />
-      ))}
+      {isSuccess && Bikes.map((bike) => <Bike bike={bike} key={bike.id} />)}
       {isError && error.toString()}
     </div>
-  )
-}
+  );
+};
 
-export default BikeList
+export default BikeList;
