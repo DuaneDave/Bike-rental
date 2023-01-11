3e9228125f95e9cbc42b7f3f59a7b30710b07627
duanedave <duanedave17@gmail.com>
2023-01-10 20:16:41 -0800

    Add queries for reservation

--- src/components/api/apiSlice.js
+++ src/components/api/apiSlice.js
@@ -1,14 +1,37 @@
-import {createApi, fetchBaseQuery} from '@reduxjs/toolkit/query/react'
+import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 
 export const apiSlice = createApi({
   reducerPath: 'Bikes',
-  baseQuery: fetchBaseQuery({baseUrl:'https://bike-web.herokuapp.com/api/v1'}),
-  endpoints: builder => ({
+  baseQuery: fetchBaseQuery({
+    baseUrl: 'https://bike-web.herokuapp.com/api/v1',
+  }),
+  endpoints: (builder) => ({
     getBikes: builder.query({
-      query: () => '/bikes'
-    })
-  })
-})
+      query: () => '/bikes',
+    }),
+    getUsers: builder.query({
+      query: () => '/users',
+    }),
+    addUser: builder.mutation({
+      query: (body) => ({
+        url: '/users',
+        method: 'POST',
+        body: body,
+      }),
+    }),
+    addNewReservation: builder.mutation({
+      query: (body) => ({
+        url: '/reservations',
+        method: 'POST',
+        body: body,
+      }),
+    }),
+  }),
+});
 
-export const { useGetBikesQuery } = apiSlice
-// https://bike-web.herokuapp.com/api/v1/reservations
+export const {
+  useGetBikesQuery,
+  useGetUsersQuery,
+  useAddUserMutation,
+  useAddNewReservationMutation,
+} = apiSlice;
