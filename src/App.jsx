c6568d204ba7e954d91de2a9f98bda73d48f7700
duanedave <duanedave17@gmail.com>
2023-01-10 20:29:24 -0800

    Protect pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,31 @@
-import React from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import LogIn from "./pages/LogIn";
-import Sidebar from "./components/sideBar/Sidebar";
-import SignUp from "./pages/SignUp";
-import Home from "./pages/Home";
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+
+import sessionStorage from './helpers/sessions';
+
+import LogIn from './pages/LogIn';
+import Sidebar from './components/sideBar/Sidebar';
+import SignUp from './pages/SignUp';
+import Home from './pages/Home';
 import Bikes from './pages/Bikes';
 import Details from './pages/Details';
-import Reservations from "./pages/Resevation";
-import "./index.css";
+import Reservations from './pages/Resevation';
+import './index.css';
 
 function App() {
+  const user = sessionStorage('get');
+
   return (
     <React.Fragment>
       <Router>
-      <Sidebar />
+        <Sidebar />
         <Routes>
-          <Route path="/" element={<LogIn />} />
-          <Route path="/signup" element={<SignUp />} />
-          <Route path="/home" element={<Home />} />
-          <Route path="bikes"  element = {<Bikes />}/>
-          <Route path='bikes/:param' element = {<Details />} />
-          <Route path="/reserve" element={<Reservations />} />
+          <Route path='/' element={<LogIn />} />
+          <Route path='/signup' element={<SignUp />} />
+          {user && <Route path='/home' element={<Home />} />}
+          {user && <Route path='/bikes' element={<Bikes />} />}
+          {user && <Route path='/details' element={<Details />} />}
+          {user && <Route path='/reservations' element={<Reservations />} />}
         </Routes>
       </Router>
     </React.Fragment>
