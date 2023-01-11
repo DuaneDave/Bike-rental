d57bd36c9ad70d93f26916f9ac1a00c43d07a86f
duanedave <duanedave17@gmail.com>
2023-01-10 20:21:11 -0800

    Remove unused state slices

--- src/app/store.js
+++ src/app/store.js
@@ -2,13 +2,10 @@ import { configureStore } from '@reduxjs/toolkit';
 import { apiSlice } from '../components/api/apiSlice';
 import bikeSlice from '../components/bikes/bikeSlice';
 
-import userSlice from '../components/userSlice';
-
 const store = configureStore({
   reducer: {
     [apiSlice.reducerPath]: apiSlice.reducer,
     toggelDetails: bikeSlice.reducer,
-    users: userSlice,
   },
   middleware: (getDefaultMiddlewear) =>
     getDefaultMiddlewear().concat(apiSlice.middleware),
