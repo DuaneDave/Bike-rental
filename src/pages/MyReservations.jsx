b30571e8af46d9961b1d3dc5178ac166da3815e0
Anas <anas.chaabane98@gmail.com>
2023-01-12 10:11:08 +0100

    reservations showing

--- src/pages/MyReservations.jsx
+++ src/pages/MyReservations.jsx
@@ -1,9 +1,27 @@
-import React from "react";
-
+import { useGetReservationsQuery } from "../components/api/apiSlice";
+import Spiner from "../reusables/spiner/Spinner";
 const MyReservations = () => {
+  const {
+    data: reservations,
+    isLoading,
+    isSuccess,
+    isError,
+    isFetching,
+    error,
+  } = useGetReservationsQuery();
+
   return (
     <div>
       <h1>My Reservations</h1>
+      {isLoading && <Spiner />}
+      {isSuccess &&
+        reservations.map((res) => (
+          <div key={res.id}>
+            <p>{res.reservation_date}</p>
+            <p>{res.bike_id}</p>
+          </div>
+        ))}
+      {isFetching && <Spiner />}
     </div>
   );
 };
--- src/pages/Resevation.jsx
+++ src/pages/Resevation.jsx
@@ -1,38 +1,39 @@
-import UseChange from '../hooks/UseChange';
+import UseChange from "../hooks/UseChange";
 import {
   useAddNewReservationMutation,
   useGetBikesQuery,
-} from '../components/api/apiSlice';
+} from "../components/api/apiSlice";
 
-import sessionStorage from '../helpers/sessions';
+import sessionStorage from "../helpers/sessions";
 
-import Container from '../reusables/container/Container';
-import Input, { Select } from '../reusables/inputFields/Inputs';
+import Container from "../reusables/container/Container";
+import Input, { Select } from "../reusables/inputFields/Inputs";
 
 const now = new Date();
 const today = `${now.getFullYear()}/${now.getMonth() + 1}/${now.getDate()}`;
-const user = JSON.parse(localStorage.getItem('user'));
+const user = JSON.parse(localStorage.getItem("user"));
 
 function Reservations() {
   const [reservationDate, handleReservationDateChange] = UseChange(today);
   const [dueDate, handleDueDateChange] = UseChange(today);
-  const [bike, handleBikeChange] = UseChange('');
+  const [bike, handleBikeChange] = UseChange("");
 
   const [addNewReservation] = useAddNewReservationMutation();
   const { data: bikes } = useGetBikesQuery();
 
+  console.log(bikes);
   const handleSubmit = (e) => {
     e.preventDefault();
     const selectedBike = bikes.find(
       (each) => each.id === Number(bike.slice(0, 1))
     );
 
-    const userData = sessionStorage('get');
+    const userData = sessionStorage("get");
 
     const data = {
       reservation_date: reservationDate,
       due_date: dueDate,
-      bike_id: selectedBike.id,
+      bike_id: selectedBike.name,
       user_id: userData.id,
     };
 
@@ -42,24 +43,24 @@ function Reservations() {
 
   return (
     <Container>
-      <div className='form-container flex flex-column'>
-        <span className='flex flex-column center greeting'>
+      <div className="form-container flex flex-column">
+        <span className="flex flex-column center greeting">
           <h2>Add a new Bike Resevation</h2>
         </span>
 
-        <form onSubmit={handleSubmit} className='flex flex-column'>
+        <form onSubmit={handleSubmit} className="flex flex-column">
           <Input
-            type={'date'}
-            label={'Reservation Date'}
+            type={"date"}
+            label={"Reservation Date"}
             onChange={(e) => handleReservationDateChange(e)}
             value={reservationDate}
           />
           <Input
-            type={'date'}
-            label={'Due Date'}
+            type={"date"}
+            label={"Due Date"}
             onChange={(e) => handleDueDateChange(e)}
           />
-          <Select label={'Bike'} handleChange={(e) => handleBikeChange(e)}>
+          <Select label={"Bike"} handleChange={(e) => handleBikeChange(e)}>
             {bikes.map((bike) => (
               <option key={bike.id}>
                 {bike.id}.{bike.name}
@@ -67,7 +68,7 @@ function Reservations() {
             ))}
           </Select>
 
-          <button type='submit'>Submit</button>
+          <button type="submit">Submit</button>
         </form>
       </div>
     </Container>
