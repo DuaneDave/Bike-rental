9e9cf406427af5b6a5108713cfa9c36649f88f5f
duanedave <duanedave17@gmail.com>
2023-01-13 22:38:43 -0800

    Refactor the my reservation component

--- src/pages/MyReservations.jsx
+++ src/pages/MyReservations.jsx
@@ -1,45 +1,30 @@
 import {
   useGetBikesQuery,
   useGetReservationsQuery,
-  useDeleteReservationMutation,
-} from "../components/api/apiSlice";
-import Spiner from "../reusables/spiner/Spinner";
+} from '../components/api/apiSlice';
+
+import Spiner from '../reusables/spiner/Spinner';
+import ReservationCard from '../components/my_reservation/ReservationCard';
+
 const MyReservations = () => {
-  const { data: bikes, isSuccess: Success } = useGetBikesQuery();
-  const [deleteres] = useDeleteReservationMutation();
+  const { data: bikes, isSuccess: success } = useGetBikesQuery();
+
   const {
     data: reservations,
     isLoading,
     isSuccess,
-    isError,
     isFetching,
-    error,
   } = useGetReservationsQuery();
-  console.log(bikes);
+
   return (
-    <div>
+    <>
       <h1>My Reservations</h1>
       {isLoading && <Spiner />}
-      {isSuccess &&
-        Success &&
-        reservations.map((res) => {
-          const bike = bikes.filter((bike) => bike.id === res.bike_id)[0];
-          console.log(bike);
-          return (
-            <div key={res.id}>
-              <img
-                src={bike.images[Object.keys(bike.images)[0]]}
-                style={{ width: "100px" }}
-              ></img>
-              <p>Bike: {`${bike.name} ${bike.brand}`}</p>
-              <p>Reservation date: {res.reservation_date}</p>
-              <p>City: {res.city}</p>
-              {/* <button onClick={() => deleteres(bike.id)}>Delete</button> */}
-            </div>
-          );
-        })}
+      {isSuccess && success && (
+        <ReservationCard reservations={reservations} bikes={bikes} />
+      )}
       {isFetching && <Spiner />}
-    </div>
+    </>
   );
 };
 
