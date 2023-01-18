7ed9022e4db9c99133f83a85a050bb3e0d138b2d
Anas <anas.chaabane98@gmail.com>
2023-01-18 20:53:31 +0100

    fix my reservation query

--- src/components/my_reservation/ReservationCard.jsx
+++ src/components/my_reservation/ReservationCard.jsx
@@ -1,47 +1,64 @@
 import styles from "./ReservationCard.module.css";
+import { useGetReservationsQuery } from "../api/apiSlice";
+import Spiner from "../../reusables/spiner/Spinner";
 
-function ReservationCard({ reservations, bikes }) {
+function ReservationCard() {
+  const {
+    data: reservations,
+    isLoading,
+    isSuccess,
+    isFetching,
+  } = useGetReservationsQuery();
   return (
-    <section className={styles.wrapper + " flex flex-column center"}>
-      <div className={styles.cardContainer + " grid gap"}>
-        {reservations.map((reservation) => {
-          const bike = bikes.filter(
-            (bike) => bike.id === reservation.bike_id
-          )[0];
-          return (
-            <div
-              className={styles.card + " flex flex-column gap"}
-              key={reservation.id}
-            >
-              <img src={bike.images[Object.keys(bike.images)[0]]} />
-              <div className={styles.cardDetails + " flex flex-column"}>
-                <h3 className="flex">
-                  Bike Name:
-                  <span>
-                    {bike.name}
-                    {bike.brand}
-                  </span>
-                </h3>
-                <span>
-                  Reservation date:
-                  <span>{reservation.reservation_date}</span>
-                </span>
-                <span>
-                  Due date:
-                  <span>{reservation.due_date}</span>
-                </span>
-                <span>
-                  City of reservation:
-                  <span>{reservation.city}</span>
-                </span>
-              </div>
+    <>
+      {isLoading && <Spiner />}
+      {isFetching && <Spiner />}
+      {isSuccess && (
+        <section className={styles.wrapper + " flex flex-column center"}>
+          <div className={styles.cardContainer + " grid gap"}>
+            {reservations.map((reservation) => {
+              return (
+                <div
+                  className={styles.card + " flex flex-column gap"}
+                  key={reservation.id}
+                >
+                  <img
+                    src={
+                      reservation.bike.images[
+                        Object.keys(reservation.bike.images)[0]
+                      ]
+                    }
+                  />
+                  <div className={styles.cardDetails + " flex flex-column"}>
+                    <h3 className="flex">
+                      Bike Name:
+                      <span>
+                        {reservation.bike.name}
+                        {reservation.bike.brand}
+                      </span>
+                    </h3>
+                    <span>
+                      Reservation date:
+                      <span>{reservation.reservation_date}</span>
+                    </span>
+                    <span>
+                      Due date:
+                      <span>{reservation.due_date}</span>
+                    </span>
+                    <span>
+                      City of reservation:
+                      <span>{reservation.city}</span>
+                    </span>
+                  </div>
 
-              <span></span>
-            </div>
-          );
-        })}
-      </div>
-    </section>
+                  <span></span>
+                </div>
+              );
+            })}
+          </div>
+        </section>
+      )}
+    </>
   );
 }
 
--- src/pages/MyReservations.jsx
+++ src/pages/MyReservations.jsx
@@ -1,30 +1,11 @@
-import {
-  useGetBikesQuery,
-  useGetReservationsQuery,
-} from "../components/api/apiSlice";
-
-import Spiner from "../reusables/spiner/Spinner";
 import ReservationCard from "../components/my_reservation/ReservationCard";
 
 const MyReservations = () => {
-  const { data: bikes, isSuccess: success } = useGetBikesQuery();
-
-  const {
-    data: reservations,
-    isLoading,
-    isSuccess,
-    isFetching,
-  } = useGetReservationsQuery();
-
-  console.log(reservations);
   return (
     <>
       <h1>My Reservations</h1>
-      {isLoading && <Spiner />}
-      {isSuccess && success && (
-        <ReservationCard reservations={reservations} bikes={bikes} />
-      )}
-      {isFetching && <Spiner />}
+
+      <ReservationCard />
     </>
   );
 };
