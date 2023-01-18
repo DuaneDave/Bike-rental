8e215e9dd1fb899b23e90020399b2b110d69818d
Ernest Anyewe Adonu <principlesbrothers@yahoo.com>
2023-01-18 18:13:40 +0000

    Update Bikes.test.js

--- src/__tests__/Bikes.test.js
+++ src/__tests__/Bikes.test.js
@@ -1,15 +1,13 @@
-import React from "react";
-import renderer from "react-test-renderer";
-import Bike from "../__mock__/Bike";
+import React from 'react';
+import renderer from 'react-test-renderer';
+import Bike from '../__mock__/Bike';
 
-
-describe("Bikes", () => {
-    test('if bikes mocked automatically', () => {
-        const component = renderer.create(
-            <Bike />
-        );
-        let tree = component.toJSON();
-        expect(tree).toMatchSnapshot();
-    })
-
-})
+describe('Bikes', () => {
+  test('if bikes mocked automatically', () => {
+    const component = renderer.create(
+      <Bike />,
+    );
+    const tree = component.toJSON();
+    expect(tree).toMatchSnapshot();
+  });
+});
