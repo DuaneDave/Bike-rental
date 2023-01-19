63d99d3af2f5b6d57f5faaf33e96eadbf6912b66
Duane David <duanedave17@gmail.com>
2023-01-19 05:46:10 -0800

    Fix some render issue in the sidebar

--- src/components/sideBar/Sidebar.jsx
+++ src/components/sideBar/Sidebar.jsx
@@ -1,27 +1,27 @@
-import { useState } from "react";
-import { Link, useNavigate } from "react-router-dom";
+import { useState } from 'react';
+import { Link, useNavigate } from 'react-router-dom';
 
-import { CiUser, CiLogout } from "react-icons/ci";
-import { RiReservedLine } from "react-icons/ri";
-import { AiOutlineProfile } from "react-icons/ai";
-import { BiAddToQueue } from "react-icons/bi";
-import { MdBookmarkRemove } from "react-icons/md";
+import { CiUser, CiLogout } from 'react-icons/ci';
+import { RiReservedLine } from 'react-icons/ri';
+import { AiOutlineProfile } from 'react-icons/ai';
+import { BiAddToQueue } from 'react-icons/bi';
+import { MdBookmarkRemove } from 'react-icons/md';
 
-import sessionStorage from "../../helpers/sessions";
+import sessionStorage from '../../helpers/sessions';
 
-import styles from "./Sidebar.module.css";
-import Socials from "./Socials";
-import ThemeToggle from "./themeToggle/ThemeToggle";
-import Button from "../../reusables/button/Button";
+import styles from './Sidebar.module.css';
+import Socials from './Socials';
+import ThemeToggle from './themeToggle/ThemeToggle';
+import Button from '../../reusables/button/Button';
 
 function Sidebar() {
   const [toggled, setToggled] = useState(false);
-  const avatar = JSON.parse(localStorage.getItem("user")) || {};
+  const avatar = JSON.parse(localStorage.getItem('user')) || {};
   const navigate = useNavigate();
 
   const handleLogout = () => {
-    sessionStorage("remove");
-    navigate("/");
+    sessionStorage('remove');
+    navigate('/');
     window.location.reload();
   };
 
@@ -29,17 +29,17 @@ function Sidebar() {
     <header className="flex center">
       <Button
         onClick={() => setToggled(!toggled)}
-        text={toggled ? "<<" : ">>"}
+        text={toggled ? '<<' : '>>'}
         className={toggled ? styles.move : styles.hamburger}
       />
 
-      <span className={styles.avatar + " flex center gap"}>
+      <span className={styles.avatar + ' flex center gap'}>
         {avatar.avatar ? (
-          <img src={avatar.avatar} onClick={() => navigate("/reservations")} />
+          <img src={avatar.avatar} onClick={() => navigate('/reservations')} />
         ) : (
           <span
             className="flex center"
-            onClick={() => navigate("/reservations")}
+            onClick={() => navigate('/reservations')}
           >
             <CiUser />
           </span>
@@ -48,31 +48,51 @@ function Sidebar() {
       </span>
       <nav
         className={
-          toggled ? styles.toggle + " flex flex-column" : "flex flex-column"
+          toggled ? styles.toggle + ' flex flex-column' : 'flex flex-column'
         }
       >
-        <Link to="/home" className={styles.logo}>
+        <Link
+          onClick={() => setToggled(!toggled)}
+          to="/home"
+          className={styles.logo}
+        >
           Trek
         </Link>
         <ul className="flex flex-column">
           <li>
-            <Link to="/reserve" className="flex gap">
+            <Link
+              onClick={() => setToggled(!toggled)}
+              to="/reserve"
+              className="flex gap"
+            >
               <RiReservedLine /> Reserve
             </Link>
           </li>
           <li>
-            <Link to="/reservations" className="flex gap">
+            <Link
+              onClick={() => setToggled(!toggled)}
+              to="/reservations"
+              className="flex gap"
+            >
               <AiOutlineProfile /> My Reservations
             </Link>
           </li>
           <li>
-            <Link to="/add_bike" className="flex gap">
+            <Link
+              onClick={() => setToggled(!toggled)}
+              to="/add_bike"
+              className="flex gap"
+            >
               <BiAddToQueue />
               Add a bike
             </Link>
           </li>
           <li>
-            <Link to="/delete_bike" className="flex gap">
+            <Link
+              onClick={() => setToggled(!toggled)}
+              to="/delete_bike"
+              className="flex gap"
+            >
               <MdBookmarkRemove />
               Remove a bike
             </Link>
@@ -80,7 +100,7 @@ function Sidebar() {
         </ul>
 
         <span
-          className={styles.logout + " flex center gap"}
+          className={styles.logout + ' flex center gap'}
           onClick={handleLogout}
         >
           <CiLogout />
