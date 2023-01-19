8c74d7bd60a4cd72d7e69835c624f32a18fc0647
Duane David <duanedave17@gmail.com>
2023-01-19 05:49:48 -0800

    Update the myreservation semantics and layout

--- src/pages/MyReservations.jsx
+++ src/pages/MyReservations.jsx
@@ -3,8 +3,6 @@ import ReservationCard from "../components/my_reservation/ReservationCard";
 const MyReservations = () => {
   return (
     <>
-      <h1>My Reservations</h1>
-
       <ReservationCard />
     </>
   );
